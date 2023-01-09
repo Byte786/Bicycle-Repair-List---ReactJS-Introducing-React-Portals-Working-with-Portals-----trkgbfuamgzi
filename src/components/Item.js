c6a6435380476ef7f6f1d7700abb4c6c0b0d7a55
RAGHURAJ SINGH RAJPOOT <114226990+Raghuraj361@users.noreply.github.com>
2023-01-09 12:50:49 +0530

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,32 @@
 import React from "react";
-import Item from "./Item";
-import { useSelector } from "react-redux";
+import { repairRemoved, repairResolved, editTask } from "../actions";
+import { useDispatch } from "react-redux";
 
-export default function List() {
-  const items = useSelector((state) => state.bicycle.items);
+export default function Item({ item }) {
+  const { id, owner, model, description, resolved } = item;
+  const dispatch = useDispatch();
+  const handleUpdate = () => {
+    dispatch(
+      editTask({
+        id: id,
+        owner: owner,
+        model: model,
+        description: description,
+        resolved: resolved,
+      })
+    );
+  };
   return (
-    <div>
-      {items.map((item) => {
-        return <Item item={item} key={item.id} />;
-      })}
+    <div className="repair-item">
+      <span>{owner}</span>
+      <span>{model}</span>
+      <span>{description}</span>
+      {"-------- "}
+      <button onClick={handleUpdate}>Update</button>
+      <button onClick={(e) => dispatch(repairRemoved(id))}>Delete</button>
+      <button onClick={(e) => dispatch(repairResolved(id))}>
+        {!resolved ? "Done" : "Undo"}
+      </button>
     </div>
   );
 }
